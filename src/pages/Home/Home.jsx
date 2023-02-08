61271888f883056385280cc3a0f4f53061480aa5
Park Sohee <pssh5124@gmail.com>
2023-02-08 16:41:53 +0900

    feat:컨설팅 섹션 완성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CurriculumWrap from '@/components/home/curriculum/CurriculumWrap';
+import ConsultingWrap from '@/components/home/consulting/ConsultingWrap';
 
 export default function Home() {
 	return (
 		<>
 			<CurriculumWrap />
+			<ConsultingWrap />
 		</>
 	);
 }
