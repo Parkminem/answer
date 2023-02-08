51d58d5de5f7d38f9da0cbb02fa0e9c9fda9bc21
Park Sohee <pssh5124@gmail.com>
2023-02-08 17:37:42 +0900

    feat : 리뷰섹션 완성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CurriculumWrap from '@/components/home/curriculum/CurriculumWrap';
 import ConsultingWrap from '@/components/home/consulting/ConsultingWrap';
+import ReviewWrap from '@/components/home/review/ReviewWrap';
 
 export default function Home() {
 	return (
 		<>
 			<CurriculumWrap />
 			<ConsultingWrap />
+			<ReviewWrap />
 		</>
 	);
 }
