52bcf11f7626420c8407b9af09c77636bbdea1f9
Minhyuk Park <comon23463@gmail.com>
2023-02-08 15:39:42 +0900

    header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import style from '@/components/common/Header.module.scss';
+import logo from '@/assets/images/common/logo.png';
 
 export default function Header() {
-	return <div>Header</div>;
+	return (
+		<div className={style.main_logo}>
+			<span className={style.logo}>
+				<img src={logo} alt="logo" />
+			</span>
+			<span className={style.title}>answer</span>
+		</div>
+	);
 }
