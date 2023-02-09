b2bd075bc79ecb445ecb09e5d50dcb4e447763e2
Park Sohee <pssh5124@gmail.com>
2023-02-09 17:38:14 +0900

    feat : 설문 api 추가

--- src/apis/api/auth.js
+++ src/apis/api/auth.js
@@ -4,6 +4,7 @@ import defaultInstance from '@/apis/utils/defaultInstance';
  * 로그인 api
  * @param {json}userInfo email, password
  * @returns header(code, message), body(...additionProp)
+ * @author sohee
  */
 function getLogin(userInfo) {
 	return defaultInstance.post('/api/v1/login', userInfo);
@@ -12,6 +13,7 @@ function getLogin(userInfo) {
 /**
  * 리프레쉬 토큰으로 액세스 토큰 재발급 api
  * @returns header(code, message), body(...additionProp)
+ * @author sohee
  */
 function getRefresh() {
 	return defaultInstance.get('/api/v1/refresh');
