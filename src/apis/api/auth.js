1d60ae7b698c9ff058fa57fb8e0fc8fa9218090b
Park Sohee <pssh5124@gmail.com>
2023-02-09 17:08:16 +0900

    feat : authApi 추가

--- src/apis/api/auth.js
+++ src/apis/api/auth.js
@@ -8,3 +8,18 @@ import defaultInstance from '@/apis/utils/defaultInstance';
 function getLogin(userInfo) {
 	return defaultInstance.post('/api/v1/login', userInfo);
 }
+
+/**
+ * 리프레쉬 토큰으로 액세스 토큰 재발급 api
+ * @returns header(code, message), body(...additionProp)
+ */
+function getRefresh() {
+	return defaultInstance.get('/api/v1/refresh');
+}
+
+const authApi = {
+	getLogin,
+	getRefresh,
+};
+
+export default authApi;
