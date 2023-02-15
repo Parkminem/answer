17255b9396ff4c2e1d894f6e2caae9237d4d6ec7
Park Sohee <pssh5124@gmail.com>
2023-02-15 16:21:14 +0900

    면접 탭 퍼블리싱 완료

---
+++ src/components/InterviewTab.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import { useLocation, Link } from 'react-router-dom';
+import styles from '@/components/InterviewTab.module.scss';
+
+const InterviewTab = () => {
+	const cx = classNames.bind(styles);
+	const { pathname } = useLocation();
+	return (
+		<div className={cx('interview-tap')}>
+			<div className={cx('interview-tap__desc')}>
+				<Link to="">
+					<span>면접 진단 내용</span>
+				</Link>
+			</div>
+			<div className={cx('interview-tap__feedback', pathname === '/mypage/feedback' ? 'active' : '')}>
+				<Link to="/mypage/feedback">
+					<span>면접 피드백</span>
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default InterviewTab;
