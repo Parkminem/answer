bc00c463dde34474b7218ebb2f1fcd20085f4bfb
pparksso <thgml5143@hanmail.net>
2023-02-27 11:59:50 +0900

    모바일 홈화면 준비섹션 퍼블리싱 완료

--- src/components/home/ready/Ready.jsx
+++ src/components/home/ready/Ready.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '@/components/home/ready/Ready.module.scss';
 import ready_strong from '@/assets/images/image/ready_strong.png';
 import ready01 from '@/assets/images/image/ready01.png';
+import mobileBg from '@/assets/images/mobile/ready_section.png';
 
 const Ready = () => {
+	const width = window.innerWidth;
+	const [mobile, setMobile] = useState(width);
 	return (
-		<section className={style.ready}>
-			<div className={style.container}>
-				<div className={style.left_wrap}>
-					<img src={ready_strong} alt="면접 준비 강조점" />
-					<div className={style.title}>면접 준비</div>
-					<div className={style.sub_title}>
-						<p>어떻게 준비해야 할지</p>
-						<strong>모르겠다면?</strong>
+		<>
+			{mobile < 401 ? (
+				<section style={{ width: 100 + '%' }}>
+					<img src={mobileBg} alt="면접 준비 이미지" style={{ objectFit: 'contain' }} />
+				</section>
+			) : (
+				<section className={style.ready}>
+					<div className={style.container}>
+						<div className={style.left_wrap}>
+							<img src={ready_strong} alt="면접 준비 강조점" />
+							<div className={style.title}>면접 준비</div>
+							<div className={style.sub_title}>
+								<p>어떻게 준비해야 할지</p>
+								<strong>모르겠다면?</strong>
+							</div>
+						</div>
+						<div className={style.right_wrap}>
+							<img src={ready01} alt="면접 준비 이미지" />
+						</div>
 					</div>
-				</div>
-				<div className={style.right_wrap}>
-					<img src={ready01} alt="면접 준비 이미지" />
-				</div>
-			</div>
-		</section>
+				</section>
+			)}
+		</>
 	);
 };
 
