6411c4ca0cc6be2bcf60ebb0fc3f2ab1f539e17c
pparksso <thgml5143@hanmail.net>
2023-02-27 10:51:37 +0900

    모바일 홈화면 강사섹션 퍼블리싱 완료

--- src/components/home/teacherList/TeacherList.jsx
+++ src/components/home/teacherList/TeacherList.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import style from '@/components/home/teacherList/TeacherList.module.scss';
+import 'swiper/swiper.min.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import teacher01 from '@/assets/images/image/teacher01.png';
 import teacher02 from '@/assets/images/image/teacher02.png';
+import mobileBar from '@/assets/images/mobile/mobile_teacher_bar.png';
+import mobileTitle from '@/assets/images/mobile/mobile_teacher_title.png';
 import data from '@/data/data';
 import Profile from './Profile';
 import TeacherCard from './TeacherCard';
 
 const TeacherList = () => {
+	const width = window.innerWidth;
+	const [mobile, setMobile] = useState(width);
 	const teacherList = data.teacherList;
 	const [selectedTeacher, setSelectedTeacher] = useState(teacherList[1]);
 	const [isSelected, setIsSelected] = useState(false);
@@ -19,11 +25,23 @@ const TeacherList = () => {
 		});
 	};
 
+	const swiperParams = {
+		centerInsufficientSlides: true,
+		slidesPerView: 4.3,
+	};
+	const swiperStyle = {
+		overflow: 'visible',
+	};
+
 	return (
 		<section className={style.teacher}>
-			<img src={teacher01} alt="상단바" style={{ width: 100 + '%' }} />
+			{mobile < 401 ? (
+				<img src={mobileBar} alt="상단바" style={{ width: 100 + '%' }} className={style.bar} />
+			) : (
+				<img src={teacher01} alt="상단바" style={{ width: 100 + '%' }} />
+			)}
 			<div className={style.container}>
-				<img src={teacher02} alt="title" />
+				{mobile < 401 ? <img src={mobileTitle} alt="title" /> : <img src={teacher02} alt="title" />}
 				<div className={style.profile}>
 					<Profile
 						key={selectedTeacher.name}
@@ -33,15 +51,27 @@ const TeacherList = () => {
 						recode={selectedTeacher.recode}
 					/>
 				</div>
-				<ul className={style.teacher_list}>
-					{teacherList.map((teacher, idx) => {
-						return (
-							<li key={teacher.name} onClick={() => selectTeacher(idx)}>
-								<TeacherCard imageList={teacher.image_list} isSelected={teacher.is_selected} />
-							</li>
-						);
-					})}
-				</ul>
+				{mobile < 401 ? (
+					<Swiper className={style.teacher_list} {...swiperParams} style={swiperStyle}>
+						{teacherList.map((teacher, idx) => {
+							return (
+								<SwiperSlide key={teacher.name} onClick={() => selectTeacher(idx)}>
+									<TeacherCard imageList={teacher.image_list} isSelected={teacher.is_selected} />
+								</SwiperSlide>
+							);
+						})}
+					</Swiper>
+				) : (
+					<ul className={style.teacher_list}>
+						{teacherList.map((teacher, idx) => {
+							return (
+								<li key={teacher.name} onClick={() => selectTeacher(idx)}>
+									<TeacherCard imageList={teacher.image_list} isSelected={teacher.is_selected} />
+								</li>
+							);
+						})}
+					</ul>
+				)}
 			</div>
 		</section>
 	);
